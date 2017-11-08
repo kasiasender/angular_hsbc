2161156bd5f2c73f3430cdfbeb2e9f24462674e6
Tomek Sułkowski <tomek.sulkowski@gmail.com>
2017-11-08 08:44:16 +0100

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'tm-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'tm';
+
+  handleClick(event: MouseEvent) {
+    this.title = this.title + event.clientX;
+  }
+}
