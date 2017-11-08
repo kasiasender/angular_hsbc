6e3f1b9edcba4071fa6820154737b79686362bfe
Tomek Sułkowski <tomek.sulkowski@gmail.com>
2017-11-08 13:36:52 +0100

    tvmaze service with HttpClient

--- src/app/tv/tv.module.ts
+++ src/app/tv/tv.module.ts
@@ -1,13 +1,13 @@
 import { TvmazeService } from './tvmaze.service';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HttpModule } from '@angular/http';
+import { HttpClientModule } from '@angular/common/http';
 import { SearchComponent } from './search/search.component';
 
 @NgModule({
   imports: [
     CommonModule,
-    HttpModule
+    HttpClientModule
   ],
   providers: [
     TvmazeService
--- src/app/tv/tvmaze.service.ts
+++ src/app/tv/tvmaze.service.ts
@@ -2,17 +2,17 @@ import { Http } from '@angular/http';
 import { Show, ShowResponse } from './tv.models';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable()
 export class TvmazeService {
   private readonly baseUrl = 'https://api.tvmaze.com/';
 
-  constructor(private http: Http) { }
+  constructor(private http: HttpClient) { }
 
   seachShows(query: string): Observable<Show[]> {
     const url = `${this.baseUrl}search/shows?q=${query}`;
-    return this.http.get(url)
-      .map(resp => resp.json() as ShowResponse[])
+    return this.http.get<ShowResponse[]>(url)
       .map(showsResponse => showsResponse.map(({show}) => show));
   }
 
