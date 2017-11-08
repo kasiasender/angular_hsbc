d82b4a48cfdc17c700de60a4670d30a91fd57156
Tomek Sułkowski <tomek.sulkowski@gmail.com>
2017-11-08 10:59:27 +0100

    ngFor in menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
 
+interface MenuItem {
+  path: string;
+  label: string;
+  exact?: boolean;
+}
+
 @Component({
   selector: 'tm-root',
   templateUrl: './app.component.html',
@@ -7,6 +13,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'tm';
+  menu: MenuItem[] = [
+    {path: '/', label: 'Home', exact: true},
+    {path: '/tv', label: 'TV Shows'},
+    {path: '/contact', label: 'Contact'}
+  ];
 
   handleClick(event: MouseEvent) {
     this.title = this.title + event.clientX;
