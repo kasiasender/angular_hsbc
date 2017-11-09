a981dcfa4cf3efd0049a6e2ff8cba4020c724a23
Tomek Sułkowski <tomek.sulkowski@gmail.com>
2017-11-09 10:04:44 +0100

    using bookmarks service in search component

--- src/app/tv/search/search.component.ts
+++ src/app/tv/search/search.component.ts
@@ -1,3 +1,5 @@
+import { Bookmark } from './../../bookmarks/bookmarks.models';
+import { BookmarksService } from './../../bookmarks/bookmarks.service';
 import { TvmazeService } from './../tvmaze.service';
 import { Show, ShowResponse } from './../tv.models';
 import { Http } from '@angular/http';
@@ -13,7 +15,8 @@ export class SearchComponent implements OnInit {
   shows: Show[] = [];
   query = 'flash';
 
-  constructor(private tv: TvmazeService) {
+  constructor(private tv: TvmazeService,
+    private bs: BookmarksService) {
     this.search(this.query);
   }
 
@@ -23,6 +26,18 @@ export class SearchComponent implements OnInit {
   search(query: string) {
     this.tv.seachShows(query)
       .subscribe(shows => this.shows = shows);
+  }
+
+  saveBookmark(bookmark: Show) {
+    this.bs.add(bookmark);
+  }
+
+  isBookmarked(bookmark: Show): boolean {
+    return this.bs.has(bookmark.id);
+  }
+
+  get bookmarks(): Show[] {
+    return this.bs.getAll() as Show[];
   }
 
 }
