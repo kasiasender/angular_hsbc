0a75d82f5d8147166e1358af8f8a40b08fe6bcba
Tomek Sułkowski <tomek.sulkowski@gmail.com>
2017-11-09 13:13:11 +0100

    ngOnChanges and ChangeDetection

--- src/app/tv/poster/poster.component.ts
+++ src/app/tv/poster/poster.component.ts
@@ -1,30 +1,28 @@
 import { Show } from './../tv.models';
-import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, Input, ViewEncapsulation, OnChanges, ChangeDetectionStrategy } from '@angular/core';
 import { get } from 'lodash';
 
 @Component({
   selector: 'tm-poster',
   templateUrl: './poster.component.html',
   styleUrls: ['./poster.component.css'],
-  encapsulation: ViewEncapsulation.Emulated
+  encapsulation: ViewEncapsulation.Emulated,
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class PosterComponent implements OnInit {
+export class PosterComponent implements OnChanges {
   @Input() show: Show;
   @Input() size = 'md';
   posterUrl: string;
   private placeholderUrl = 'http://powerpeople.club/wp-content/uploads/2014/02/default-placeholder-1024x1024-570x570.png';
 
-  constructor() {
-  }
-
-  ngOnInit() {
+  ngOnChanges() {
     const sizesDict = {
       md: 'medium',
       lg: 'original'
     };
     const sizeKey = sizesDict[this.size];
 
-    this.posterUrl = get(this.show.image, [sizeKey], this.placeholderUrl);
+    this.posterUrl = get(this.show, ['image', sizeKey], this.placeholderUrl);
   }
 
 }
